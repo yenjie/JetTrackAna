1c46536b7c8d878eec8e5bd19cfe315890a51bc8
Yen-Jie Lee <yenjie@mit.edu>
2015-01-14 08:41:39 +0100

    update

--- mptCheck/plotSwap.C
+++ mptCheck/plotSwap.C
@@ -10,9 +10,14 @@
 #include <TLegend.h>
 #include "../jettrkcorr/hiForest.h"
 
+
 void plotSwap()
 {
-   TFile *inf = new TFile("../mptTree-merged.root");
+//   TFile *inf = new TFile("../mptTree-FFCor-merged.root");
+//   TFile *inf = new TFile("../mptTree-80.root");
+//   TFile *inf = new TFile("../mptTree-merged-jec1212.root");
+//     TFile *inf = new TFile("../mptTree-80-R5.root");
+     TFile *inf = new TFile("../mptTree-11080-R2.root");
    TTree *t = (TTree*)inf->Get("t");
    TFile *outf = new TFile("swapPlot.root","recreate");
    const int nCentBins=5;
@@ -21,16 +26,16 @@ void plotSwap()
    
 
    // initialize histograms   
-   
+   t->SetAlias("LD","leadingJetPt*0.00760+leadingJetNTrk4*(-0.00304)+subleadingJetPt*(-0.00883)+subleadingJetNTrk4*0.01045-0.0272");
    for (int i=0;i<nCentBins;i++) {
       TProfile *pLeading = new TProfile(Form("pLeading%d",i),"",50,0,1);
       TCanvas *c = new TCanvas(Form("c%i",i),Form("Leading Jet Matching Probability %d-%d",centBins[i],centBins[i+1]),600,600);
       pLeading->SetXTitle("Reco A_{J}");
       pLeading->SetYTitle("Leading Jet Matching Prob");
       TCut centCut = Form("%d<=hiBin&&hiBin<%d",centBins[i],centBins[i+1]);
-      t->Draw(Form("leadingJetRefPt==genleadingJetPt:Aj>>pLeading%d",i),centCut&&"leadingJetPt>120&&subleadingJetPt>50&&acos(cos(leadingJetPhi-subleadingJetPhi))>2*3.14159/3.","prof");
+      t->Draw(Form("(leadingJetRefPt==genleadingJetPt)*(LD>-0.2)+(LD<-0.2)*(subleadingJetRefPt==genleadingJetPt):Aj>>pLeading%d",i),centCut&&"leadingJetPt>120&&subleadingJetPt>50&&acos(cos(leadingJetPhi-subleadingJetPhi))>2*3.14159/3.","prof");
       // matching probability between leading reco jet and leading gen jet
-      TF1 *fPLeading = new TF1(Form("fPLeading_%d_%d",centBins[i],centBins[i+1]),"TMath::Erf(x*[0]+[1])");
+      TF1 *fPLeading = new TF1(Form("fPLeading_%d_%d",centBins[i],centBins[i+1]),"TMath::Erf(x*[0])*0.5+0.5");
       pLeading->Fit(Form("fPLeading_%d_%d",centBins[i],centBins[i+1]));
       pLeading->Fit(Form("fPLeading_%d_%d",centBins[i],centBins[i+1]));
       pLeading->Fit(Form("fPLeading_%d_%d",centBins[i],centBins[i+1]));
